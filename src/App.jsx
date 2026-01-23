23b0ffd2422232795acd89638b768f1585804a12
Thananon Chounudom <thananonchounudom@Thananons-MacBook-Pro.local>
2026-01-23 14:55:18 +0700

    Show API message on Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import TestApi from './TestApi';
 
 
+import { useEffect, useState } from 'react';
+
 function Home() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/hello')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
+      <p>Message: {message}</p>
       <Link to="/test_api">
         <button>Go to Test API</button>
       </Link>
